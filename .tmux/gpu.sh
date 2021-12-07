922e4918bbcb35b9719622380a09c0cf61ef97c5
Zhuo Zhang <zhan3299@purdue.edu>
2021-12-06 20:59:12 -0500

    support avaiable GPUs

---
+++ .tmux/gpu.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+print_gpu_usage() {
+    gpuUsage=($(nvidia-smi -q -d UTILIZATION | grep Gpu | awk '{printf "%d\n", $3}'))
+    memUsage=($(nvidia-smi | sed -nr 's/.*\s([0-9]+)MiB\s*\/\s*([0-9]+)MiB.*/\1 \2/p'  | awk -v format="%2.f\n" '{printf format, 100*$1/$2}'))
+    ids=("⓪ " "① " "② " "③ " "④ " "⑤ " "⑥ " "⑦ ")
+
+    output=""
+    for index in ${!gpuUsage[*]}; do
+        if [ "${gpuUsage[$index]}" != "100" ]; then
+            mem=$(printf "%02s" ${memUsage[$index]})
+            gpu=$(printf "%02s" ${gpuUsage[$index]})
+            if [[ "$mem" < "20" && "$gpu" < "10" ]]; then
+                output+=$(printf "%s " ${ids[$index]})
+            fi
+        fi
+    done
+
+    if [ -z "$output" ]; then
+    echo "GPU: FULL "
+    else
+    echo "GPU: $output"
+    fi
+}
+
+main() {
+  print_gpu_usage
+}
+
+main
--- README.md
+++ README.md
@@ -4,9 +4,6 @@
 ```bash
 $ git clone https://github.com/ZhangZhuoSJTU/easy_tmux.git
 $ cd easy_tmux
-$ cp .tmux.conf ~/.tmux.conf
+$ cp .tmux.conf ~
+$ cp -r .tmux ~
 ```
-
-## Troubleshoot
-
-`easy_tmux` requires powerline symbols. If you did not install any fonts supporting powerline symbols on your local machine, [Cascadia Code PL](https://github.com/microsoft/cascadia-code) is recommended. More choices can be found at [nerd-fonts](https://github.com/ryanoasis/nerd-fonts).
